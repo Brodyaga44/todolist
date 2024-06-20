87e12437319b67d2d67ff8d5b7ba6d88072b33e9
Brodyaga44 <vdon_hiznyakov_dmitriy@mail.ru>
2024-06-20 11:32:12 +0300

    init

---
+++ src/widgets/Welcome/welcome.tsx
@@ -0,0 +1,12 @@
+import styles from "./welcome.module.scss"
+const Welcome = () => {
+    return(
+        <div className={styles.welcome}>
+            <div className={styles.welcome__title}>
+                TaskDo
+            </div>
+        </div>
+        )
+}
+
+export default Welcome
